b89ca4c1e7278e5e5d71cdc7cc095adc0da79ff8
jrgdonoso <jrgdonoso@users.noreply.github.com>
2020-07-16 16:56:08 -0400

    Update peliculas.json.ts
    update de prueba para ver cambios en pages

--- src/app/pelicula/peliculas.json.ts
+++ src/app/pelicula/peliculas.json.ts
@@ -5,4 +5,5 @@ export const PELICULAS: Pelicula[] = [
   { id: 1, nombre: 'Unidos', director: 'Dan Scanlon', anioEstreno:  2020},
   { id: 2, nombre: 'Parasitos', director: 'Bong Joon-ho', anioEstreno: 2019 },
   { id: 3, nombre: 'Sonic la pelicula', director: 'Jeff Fowler', anioEstreno: 2020},
+   { id: 4, nombre: 'Bloodshot', director: 'David S. F. Wilson', anioEstreno: 2020},
   ];
